4b86d2e0aad49a7924acd034defe93aead2626d9
MossimoB <2585451@edu.vaniercollege.qc.ca>
2025-12-09 15:33:15 -0500

    started Student class

--- src/main/java/org/mossimo/Student.java
+++ src/main/java/org/mossimo/Student.java
@@ -1,5 +1,26 @@
 package org.mossimo;
 
-public class Student {
+import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import java.util.ArrayList;
 
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    private static int nextId = 1;
+
+    public enum Gender {
+      MALE, FEMALE
+    }
 }
