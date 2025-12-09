c7dd0dfa18954c364f199b439e6169912a57d6b9
MossimoB <2585451@edu.vaniercollege.qc.ca>
2025-12-09 15:10:50 -0500

    removed toTitleCase line

--- src/main/java/org/mossimo/Department.java
+++ src/main/java/org/mossimo/Department.java
@@ -18,15 +18,14 @@ public class Department {
     private static int nextId = 1;
 
     public Department(String departmentName) {
-        if (isDepartmentNamevalid(departmentName)) {
-            this.departmentName = toTitleCase(departmentName);
+        if (isDepartmentNameValid(departmentName)) {
             this.departmentId = String.format("D%02d", nextId++);
         } else {
             this.departmentName = null;
             this.departmentId = null;
         }
     }
-    
+
     public class boolean isDepartmentNameValid(String departmentName) {
         if (departmentName == null || departmentName.isEmpty()) {
             return false;
