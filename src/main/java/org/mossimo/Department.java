b7872d15e745131865b32e888e9981666a119b1b
MossimoB <2585451@edu.vaniercollege.qc.ca>
2025-12-09 15:00:36 -0500

    created Department constructor. Also imported lombok getter, setter, tostring, equalsandhashcode

--- src/main/java/org/mossimo/Department.java
+++ src/main/java/org/mossimo/Department.java
@@ -1,11 +1,32 @@
 package org.mossimo;
 
+import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Department {
 
     private String departmentId;
     private String departmentName;
-    private int nextId;
+    private static int nextId = 1;
 
+    public Department(String departmentName) {
+        if (isDepartmentNamevalid(departmentName)) {
+            this.departmentName = toTitleCase(departmentName);
+            this.departmentId = String.format("D%02d", nextId++);
+        } else {
+            this.departmentName = null;
+            this.departmentId = null;
+        }
+    }
+    
     public class boolean isDepartmentNameValid(departmentName String) {
         
     }
