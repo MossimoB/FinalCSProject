a761517a18d9c9877c5fd68b0535160908d78ea8
MossimoB <2585451@edu.vaniercollege.qc.ca>
2025-12-11 11:15:16 -0500

    created basic fields

--- src/main/java/org/mossimo/Assignment.java
+++ src/main/java/org/mossimo/Assignment.java
@@ -5,7 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 import java.util.ArrayList;
 
+@Getter
+@Setter
 public class Assignment {
+
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+    private static int nextId = 1;
   
 }
-
